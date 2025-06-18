d67c78386e6eb20163671358db069468d9620244
niko-jb <nikolay.nikolaev@jb-support.de>
2025-06-18 12:22:35 +0200

    Remove boot methond and add routing.loader to services.yaml

--- src/JBSupportMultipleSitemapsBundle.php
+++ src/JBSupportMultipleSitemapsBundle.php
@@ -6,10 +6,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class JBSupportMultipleSitemapsBundle extends Bundle
 {
-    public function boot(): void
-    {
-        if (class_exists(AnnotationRegistry::class)) {
-            AnnotationRegistry::registerLoader('class_exists');
-        }
-    }
 }
