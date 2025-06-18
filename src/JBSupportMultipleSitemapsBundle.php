83380a9df3db64069e08b44b11dae4144d214048
niko-jb <nikolay.nikolaev@jb-support.de>
2025-06-18 11:48:42 +0200

    Add boot method to register AnnotationRegistry loader

--- src/JBSupportMultipleSitemapsBundle.php
+++ src/JBSupportMultipleSitemapsBundle.php
@@ -1,8 +1,15 @@
 <?php
+
 namespace JBSupport\MultipleSitemapsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class JBSupportMultipleSitemapsBundle extends Bundle
 {
+    public function boot(): void
+    {
+        if (class_exists(AnnotationRegistry::class)) {
+            AnnotationRegistry::registerLoader('class_exists');
+        }
+    }
 }
