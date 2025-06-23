bcad00c46956bb77d4149365b58195694e517403
niko-jb <nikolay.nikolaev@jb-support.de>
2025-06-23 09:01:57 +0200

    Fix import statement for Route annotation in MultipleSitemapController

--- src/Controller/MultipleSitemapController.php
+++ src/Controller/MultipleSitemapController.php
@@ -22,7 +22,7 @@ use JBSupport\MultipleSitemapsBundle\MultipleSitemapsConfig;
 use JBSupport\MultipleSitemapsBundle\Routing\RegisterSitemapRoutes;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 #[Route(defaults: ['_scope' => 'frontend'])]
